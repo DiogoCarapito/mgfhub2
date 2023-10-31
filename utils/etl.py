5c6b3255f5d58b152762fc779685e56ac37cf1e1
Diogo Carapito <43778648+DiogoCarapito@users.noreply.github.com>
2023-10-31 12:16:34 +0000

    upload big fix tests with openpyxl

--- utils/etl.py
+++ utils/etl.py
@@ -3,13 +3,14 @@ ETL pipeline for data processing
 """
 
 import pandas as pd
+import openpyxl
 
 
 def etl_xlsx(file_xlsx):
     if file_xlsx is not None:
         file_xlsx = pd.read_excel(file_xlsx, engine="openpyxl")
         
-        
+
         
         return file_xlsx
     else:
